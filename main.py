c232e7c984cd555b2c3bcab321b68fabc5560462
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-06-22 21:34:27 +0200

    Change module name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import data_access
+import data
 import presentation
 from data_models import Station, Measure, Base
 from sqlalchemy import create_engine
@@ -12,7 +12,7 @@ conn = engine.connect()
 
 
 if __name__ == "__main__":
-    # data_access.populate_db(session, data_access.initial_data)
+    # data.populate_db(session, data_access.initial_data)
     query_data = session.query(Station).order_by(Station.id) 
     presentation.display_stations_with_measures(query_data, 3)
     result = conn.execute("SELECT * FROM stations LIMIT 5").fetchall()
